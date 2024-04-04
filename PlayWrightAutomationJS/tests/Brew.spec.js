3a5a620bd396a37a6fcd3ac836e35c8d5c10a3b3
cesarleconacastillo <36861314+cesarleconacastillo@users.noreply.github.com>
2024-04-04 14:24:37 -0600

    removing file

--- PlayWrightAutomationJS/tests/Brew.spec.js
+++
@@ -1,9 +0,0 @@
-const {test, expect, request} = require('@playwright/test');
-
-test.beforeAll( async () => {
-    const apiContext = await request.newContext();
-    const brewResponse = await apiContext.get("https://web-api.staging.bellwether.link/v1/brew-methods");
-    expect(brewResponse.ok()).toBeTruthy();
-    const brewResponseJson = brewResponse.json();
-    console.log(brewResponseJson);
-});
